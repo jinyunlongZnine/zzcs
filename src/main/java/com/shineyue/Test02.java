2f1ab91ec097422956b36ab3621b349215b4b72a
靳云龙 <jinyunlong@atwasoft.net>
2019-12-24 19:21:10 +0800

    test02    dev 提交

---
+++ src/main/java/com/shineyue/Test02.java
@@ -0,0 +1,13 @@
+package com.shineyue;
+
+/**
+ * @Companny shineyue
+ * @Author jinyunlong
+ * @Created 2019/12/24
+ * @Description test02
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("--------");
+    }
+}
