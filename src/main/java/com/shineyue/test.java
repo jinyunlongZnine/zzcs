0559c202c54e9802f1c7f6e78bb96a97c50fac44
靳云龙 <jinyunlong@atwasoft.net>
2019-12-24 18:16:06 +0800

    first commit

---
+++ src/main/java/com/shineyue/test.java
@@ -0,0 +1,13 @@
+package com.shineyue;
+
+/**
+ * @Companny shineyue
+ * @Author jinyunlong
+ * @Created 2019/12/24
+ * @Description
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
