f7b0f7f660a9656ecc6a9d38beeeef3c1c68c832
靳云龙 <jinyunlong@atwasoft.net>
2019-12-25 18:01:11 +0800

    pom  test02  提交

--- src/main/java/com/shineyue/Test02.java
+++ src/main/java/com/shineyue/Test02.java
@@ -1,5 +1,7 @@
 package com.shineyue;
 
+import org.junit.Test;
+
 /**
  * @Companny shineyue
  * @Author jinyunlong
@@ -10,5 +12,16 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("--------");
         System.out.println("args = " + args);
+
+    }
+    @Test
+    public void testSf () {
+        int a = 10;
+        int b = 11;
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < (b-a+1); j++) {
+
+            }
+        }
     }
 }
